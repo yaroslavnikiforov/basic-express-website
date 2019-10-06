38b1b7bf1682e0eb5fccb5d8727b60f42d3780d8
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-10-06 21:26:57 +0300

    Page Routes & Views

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", function(req, res) {
-  res.send("<h1>Sayonara!</h1>");
+  res.render("index", { title: "Welcome" });
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
 });
 
 app.listen(3000);
