de490ecc755b2ad41fccccedd1905776bbe4fb43
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-10-06 23:44:14 +0300

    Nodemailer Contact Form

--- index.js
+++ index.js
@@ -13,11 +13,49 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", function(req, res) {
-  res.render("index", { title: "Welcome" });
+  res.render("index", { title: "Computer not working?" });
 });
 
 app.get("/about", function(req, res) {
   res.render("about");
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact");
+});
+
+app.post("/contact/send", function(req, res) {
+  nodemailer.createTestAccount().then(testAccount => {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false,
+      auth: {
+        user: testAccount.user,
+        pass: testAccount.pass
+      }
+    });
+
+    const mailOptions = {
+      from: `Majin Buu <${testAccount.user}>`,
+      to: "mbuu8888@gmail.com",
+      subject: "Website submition",
+      text: `You have a submition with the following details... Name: ${req.body.name} Email: ${req.body.email} Message: ${req.body.message}`,
+      html: `<p>You have a submition with the following details...</p><ul><li>Name: ${req.body.name}</li><li>Email: ${req.body.email}</li><li>Message: ${req.body.message}</li></ul>`
+    };
+
+    transporter.sendMail(mailOptions, function(err, info) {
+      if (err) {
+        console.error(err);
+
+        res.redirect("/");
+      } else {
+        console.info("Message sent: ", info.response);
+
+        res.redirect("/");
+      }
+    });
+  });
 });
 
 app.listen(3000);
